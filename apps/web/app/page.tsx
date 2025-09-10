4ff7b741e1c9e3b344b03a73a1cb9b601936ae88
Hasrinata Arya Afendi <hasrinata@gmail.com>
2025-09-10 19:31:13 +0700

    feat(web): add features, process and CTA components

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,8 @@
 import Header from './components/header';
 import Hero from './components/hero';
+import Features from './components/features';
+import Process from './components/process';
+import CTA from './components/cta';
 import Footer from './components/footer';
 
 export default function LandingPage() {
@@ -8,6 +11,9 @@ export default function LandingPage() {
       <Header />
       <main className="flex-1">
         <Hero />
+        <Features />
+        <Process />
+        <CTA />
       </main>
       <Footer />
     </div>
